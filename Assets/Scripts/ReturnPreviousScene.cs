7f418b54fca17717af3bb3208dc68893fb2de31c
IsekiTakumu <kbc20b04@stu.kawahara.ac.jp>
2022-07-22 10:11:23 +0900

    アウトシーン中の戻るボタンの追加

---
+++ Assets/Scripts/ReturnPreviousScene.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+///設定シーンから前のシーン(モード選択シーン)に戻るクラス
+/// </summary>
+public class ReturnPreviousScene : MonoBehaviour
+{
+    //操作システム
+    Operation m_operation = null;
+    //前のシーン名
+    [SerializeField] string m_previousSceneName = null;
+    //設定シーンかどうか
+    [SerializeField] bool m_isSettingScene = false;
+
+    void Start()
+    {
+        //操作システムのゲームオブジェクトを検索しゲームコンポーネントを取得する
+        m_operation = GameObject.Find("OperationSystem").GetComponent<Operation>();
+    }
+
+    //前のシーンに戻る関数
+    public void GoPreviousScene()
+    {
+        //操作の判定を初期化させる
+        m_operation.TachDataInit();
+
+        //設定シーンの時のみ実行
+        if (m_isSettingScene)
+        {
+            //ブラインドモードかどうかのデータを保存
+            GameObject.Find("UserSettingDataStorageSystem").GetComponent<UserSettingData>().GetSetBlindMode = GameObject.Find("BlindToggle").GetComponent<ToggleOnOff>().GetToggleValue;
+        }
+
+        //モード選択シーンに遷移
+        SceneManager.LoadScene(m_previousSceneName);
+
+        //デバック
+        Debug.Log(m_previousSceneName + "シーンに戻ります");
+    }
+}
