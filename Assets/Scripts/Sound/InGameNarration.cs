6304c9e24310955cc48adcd721c4981b0a357235
AzumaHiroki <poohfamily1017@gmail.com>
2022-08-13 14:43:18 +0900

    壁に衝突した時、右か左か判定できるようになった。

--- Assets/Scripts/Sound/InGameNarration.cs
+++ Assets/Scripts/Sound/InGameNarration.cs
@@ -16,12 +16,22 @@ public class SoundTriggerTagName
 public class InGameNarration : MonoBehaviour
 {
 
+    //様々な判定に使う方向（左右の壁、アイテムの位置）
+    public enum EnDirection
+    {
+        enNone,     //なし
+        enRight,    //右
+        enLeft      //左
+    }
     private bool m_isOwnPlayer = false;                             //プレイヤー自身か？
     private nsSound.SoundSource m_soundSource = null;               //ナレーションのサウンドソース、重ねて再生したくないサウンドを扱うときに使用できるかも
     private ObtainItemController m_obtainItemController = null;     //プレイヤーのアイテム情報
     private bool m_getItemSoundPlayFlag = false;                    //アイテム取得時のナレーションが再生された？
     private float m_hitWallTimer = 0.0f;                            //壁に当たってから次に注意されるまでのタイマー
     private bool m_hitWall = false;                                 //壁に当たった？
+
+    //定数
+    private float m_hitWallTimerConst = 3.0f;
 
     //ObtainItemControllerのEnItemTypeがprivateでアクセスが難しかったのでとりあえず同じものを用意
     //後でうまい具合に判定できるものを用意すること。
@@ -70,7 +80,7 @@ public class InGameNarration : MonoBehaviour
         string triggerName = collision.gameObject.tag;
 
         //タグの名前からどの音源を再生するか判定して再生。
-        PlayNarration(triggerName);
+        PlayNarration(triggerName, EnDirection.enNone);
     }
 
     // 物体がコリジョン接触したとき、１度だけ呼ばれる
@@ -79,12 +89,48 @@ public class InGameNarration : MonoBehaviour
         //接触したコリジョンのタグを取得する。
         string triggerName = collision.gameObject.tag;
 
-        //タグの名前からどの音源を再生するか判定して再生。
-        PlayNarration(triggerName);
+        //衝突点
+        Vector3 contactPoint = Vector3.zero;
+        //衝突方向
+        EnDirection direction = EnDirection.enNone;
+
+        //壁に衝突していない判定の状態で壁に衝突したら
+        if (!m_hitWall && triggerName == "Wall")
+        {
+            //衝突点を取得する。
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                contactPoint = new Vector3(contact.point.x, contact.point.y, contact.point.z);
+            }
+
+            //自身から衝突点に向かって伸びるベクトルを計算。
+            Vector3 contactVec = contactPoint - this.gameObject.transform.position;
+
+            //y成分を削除。
+            contactVec.y = 0.0f;
+
+            //正規化。
+            contactVec.Normalize();
+
+            //外積を求める。
+            Vector3 dot = Vector3.Cross(contactVec, this.gameObject.transform.forward);
+
+            //外積から壁が右にあるのか左にあるのか判断する。
+            if (dot.y < 0.0)
+            {
+                direction = EnDirection.enRight;
+            }
+            else
+            {
+                direction = EnDirection.enLeft;
+            }
+        }
+        //タグの名前と衝突方向からどの音源を再生するか判定して再生。
+        PlayNarration(triggerName, direction);
     }
 
     //タグの名前から音源を再生する関数。
-    private void PlayNarration(string triggerName){
+    private void PlayNarration(string triggerName, EnDirection direction){
 
         nsSound.SoundSource ss = new GameObject("SoundSource").AddComponent<nsSound.SoundSource>();
         ss.SetSoundType(nsSound.EnSoundTypes.enNarration);
@@ -113,11 +159,20 @@ public class InGameNarration : MonoBehaviour
         {
             if (!m_hitWall)
             {
-                ss.PlayStart(nsSound.NarInGameNames.m_KABENIATATTEIRUYO);
+                //右側に当たっている。
+                if (direction == EnDirection.enRight)
+                {
+                    ss.PlayStart(nsSound.NarInGameNames.m_MIGIGAWANI);
+                }
+                //左側に当たっている。
+                else if(direction == EnDirection.enLeft)
+                {
+                    ss.PlayStart(nsSound.NarInGameNames.m_HIDARIGAWANI);
+                }
                 //壁に衝突した。
                 m_hitWall = true;
                 //タイマーをセット。
-                m_hitWallTimer = 3.0f;
+                m_hitWallTimer = m_hitWallTimerConst;
             }
         }
     }
